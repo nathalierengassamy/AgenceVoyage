a90e58c124b17354dcbe3bebf6ef3654cecd07c2
nathalierengassamy <nathalie.rengassamy@outlook.fr>
2022-06-01 15:09:24 +0200

    debut

---
+++ gestionAgenceVoyage/src/main/java/com/inti/GestionAgenceVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAgenceVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAgenceVoyageApplication.class, args);
+	}
+
+}
---
+++ gestionAgenceVoyage/src/test/java/com/inti/GestionAgenceVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAgenceVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
