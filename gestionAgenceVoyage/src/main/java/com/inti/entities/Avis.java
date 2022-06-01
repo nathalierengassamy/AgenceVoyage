8366d1eef9587fc36ec5fdc7042fe5e47a439774
nathalierengassamy <nathalie.rengassamy@outlook.fr>
2022-06-01 15:51:36 +0200

    modif avis1

--- gestionAgenceVoyage/src/main/java/com/inti/entities/Avis.java
+++ gestionAgenceVoyage/src/main/java/com/inti/entities/Avis.java
@@ -2,13 +2,8 @@ package com.inti.entities;
 
 public class Avis {
 	private Long idAvis;
-	private String commentaire;
 	
 	public Avis() {
-	}
-	
-	public Avis(String commentaire) {
-		this.commentaire = commentaire;
 	}
 
 	public Long getIdAvis() {
@@ -19,12 +14,9 @@ public class Avis {
 		this.idAvis = idAvis;
 	}
 
-	public String getCommentaire() {
-		return commentaire;
-	}
-
-	public void setCommentaire(String commentaire) {
-		this.commentaire = commentaire;
+	@Override
+	public String toString() {
+		return "Avis [idAvis=" + idAvis + "]";
 	}
 	
 }
